087582f9f7556f8ee5d88ed0bb7fc0f4821c9c88
Igor Boyko <iboyko@gmail.com>
2022-12-06 11:35:47 -0800

    18 tests total

---
+++ test/unit/mortgage.spec.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const Mortgage = require('../../src/js/lib/Mortgage');
+
+describe('Mortgage Calculator', () => {
+    let mortgage = null;
+
+    beforeEach(() => {
+        mortgage = new Mortgage(300000,3.75,30,12);
+    });
+
+    it('should have a monthly payment method', () => {
+        expect(mortgage.monthlyPayment).to.exist;
+    });
+    
+    it('should return correct monthly payment', () => {
+        expect(mortgage.monthlyPayment()).to.equal('1389.35');
+    });
+})
